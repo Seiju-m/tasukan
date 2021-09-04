2f71141324248c615e63fa8734f1fff93c6517e6
森下星需 <morishitaseiju@tfjkvarnoMacBook-Air.local>
2021-09-04 09:51:03 +0900

    default src

--- component/Layout.js
+++ component/Layout.js
@@ -2,21 +2,8 @@ import React, { memo } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Container, Header, Body, Title, Content } from "native-base";
 
-import AddTask from './AddTask';
-import { borderBottom } from 'styled-system';
-import { useInputValue, useTodos } from "../hooks/todoList";
-
 const Layout = memo((props) => {
   const { children } = props;
-  const { inputValue, changeInput, clearInput } = useInputValue();
-  const { todos, addTodo, checkTodo, removeTodo } = useTodos();
-
-  console.log(inputValue);
-
-  const clearInputAndAddTodo = _ => {
-    clearInput();
-    addTodo(inputValue);
-  };
 
   return (
     <Container>
@@ -30,8 +17,6 @@ const Layout = memo((props) => {
         {children}
       </Content>
       </View>
-
-
     </Container>
   );
 });
@@ -40,21 +25,11 @@ export default Layout;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 4,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'yellow'
   },
   content: {
     width: 350,
-    backgroundColor: 'red'
   },
-  plusIcon: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end',
-    marginRight: 20,
-    backgroundColor: 'blue',
-    borderBottomColor: 'red',
-  }
 });
